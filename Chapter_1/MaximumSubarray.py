f914f7dcdcaa6c7145b8198cb14692090bae846c
JonnesLin <1123444420@qq.com>
2020-08-18 17:31:39 +0800

    使用动态规划解决 使用分治解决

--- Chapter_1/MaximumSubarray.py
+++ Chapter_1/MaximumSubarray.py
@@ -13,16 +13,50 @@ class Solution(object):
         :rtype: int
         """
         if self.flag:
-            # 设置用于存储
+            # 用于存储信息
             saved_data = [0]
+            # 考虑边界情况
             if len(nums) == 0:
                 return 0
+            # 更新存储数据
             for index in range(len(nums)):
                 saved_data.append(max(nums[index] + saved_data[index], nums[index]))
             return max(saved_data[1:])
+        else:
+            return self.maxSubArrayByDivided(nums, 0, len(nums)-1)
+
+    def maxSubArrayByDivided(self, nums, start_index, last_index):
+        if start_index == last_index:
+            return nums[start_index]
+
+        mid_index = (start_index + last_index) // 2
+        left = self.maxSubArrayByDivided(nums, start_index, mid_index)
+        right = self.maxSubArrayByDivided(nums, mid_index + 1, last_index)
+
+        max_left_value = nums[mid_index]
+        left_sum = 0
+        # 获取从左边开始算起的最大列表
+        for index in range(mid_index, start_index-1, -1):
+            left_sum += nums[index]
+            max_left_value = max(left_sum, max_left_value)
+
+        max_right_value = nums[mid_index+1]
+        right_sum = 0
+        # 获取从右边边开始算起的最大列表
+        for index in range(mid_index+1, last_index+1):
+            right_sum += nums[index]
+            max_right_value = max(right_sum, max_right_value)
+
+        center_value = max_left_value + max_right_value
+
+        return max(center_value, left, right)
+
+
+
 
 
 if __name__ == '__main__':
-    solution = Solution(True)
+    # 当flag为True的时候，采用动态规划。当flag为False的时候，采用分治的方法
+    solution = Solution(False)
     array = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
     print(solution.maxSubArray(array))
